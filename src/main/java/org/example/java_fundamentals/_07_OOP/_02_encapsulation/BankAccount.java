e752082832fe7f64a5d54d1648114346abe51742
Danait Tesfai <dtesfai2020@gmail.com>
2025-09-24 02:49:53 -0700

    Encapsulation BankAccountRunner

---
+++ src/main/java/org/example/java_fundamentals/_07_OOP/_02_encapsulation/BankAccount.java
@@ -0,0 +1,43 @@
+package org.example.java_fundamentals._07_OOP._02_encapsulation;
+
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+    public void deposit(double amount) {
+        if (amount >= 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Put valid amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+
+}
---
+++ src/main/java/org/example/java_fundamentals/_07_OOP/_02_encapsulation/BankAccountRunner.java
@@ -0,0 +1,48 @@
+package org.example.java_fundamentals._07_OOP._02_encapsulation;
+
+import org.example.java_fundamentals._07_OOP._01_classesAndObjects._04_bankAccount.BankAccount;
+
+import java.util.Scanner;
+import java.util.SimpleTimeZone;
+
+public class BankAccountRunner {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your account number: ");
+        String accountNumber = sc.nextLine();
+        bankAccount.setAccountNumber(accountNumber);
+
+        while (true){
+            System.out.println("==== Bank Menu ====\n" +
+                    "1. Deposit\n" +
+                    "2. Withdraw\n" +
+                    "3. Check Balance\n" +
+                    "4. Exit");
+            System.out.print("Choose an option: ");
+            int choice = sc.nextInt();
+
+            if (choice == 1){
+                System.out.print("Enter deposit amount: ");
+                double amount = sc.nextDouble();
+                bankAccount.deposit(amount);
+
+            } else if (choice==2) {
+                System.out.println("Enter withdrawal amount: ");
+                double amount = sc.nextDouble();
+                bankAccount.withdraw(amount);
+
+            } else if (choice==3) {
+                System.out.println("Current Balance: $" + bankAccount.getBalance());
+
+            } else if (choice==4) {
+                System.out.println("Exiting... Thank you!");
+                break;
+            }else {
+                System.out.println("Invalid input. Please try again.");
+            }
+        }
+
+    }
+}
