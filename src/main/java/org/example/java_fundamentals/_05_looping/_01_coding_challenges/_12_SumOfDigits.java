646b8d578288abc6899a5614ba8c5e2b4b0a4417
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-02 02:46:09 -0700

    Looping SumOfDigits

--- src/main/java/org/example/java_fundamentals/_05_looping/_01_coding_challenges/_12_SumOfDigits.java
+++ src/main/java/org/example/java_fundamentals/_05_looping/_01_coding_challenges/_12_SumOfDigits.java
@@ -1,7 +1,22 @@
 package org.example.java_fundamentals._05_looping._01_coding_challenges;
 
+import java.util.Scanner;
+
 public class _12_SumOfDigits {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+            System.out.print(digit + " + ");
+        }
+        System.out.println(" = " + sum);
 
     }
 }
