89edbb50dc7aea98c7cbdca47e8be53db3bf53d7
Danait Tesfai <dtesfai2020@gmail.com>
2025-10-02 02:29:31 -0700

    Looping ReverseNumber

--- src/main/java/org/example/java_fundamentals/_05_looping/_01_coding_challenges/_09_ReverseNumber.java
+++ src/main/java/org/example/java_fundamentals/_05_looping/_01_coding_challenges/_09_ReverseNumber.java
@@ -1,7 +1,21 @@
 package org.example.java_fundamentals._05_looping._01_coding_challenges;
 
+import java.util.Scanner;
+
 public class _09_ReverseNumber {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int reversedNumber = 0;
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reversedNumber = reversedNumber * 10 + lastDigit;
+            number /= 10;
+        }
+        System.out.println("The reversed number is: "+ reversedNumber);
 
     }
 }
